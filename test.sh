5e3ebbccabecf89e0ad6e779cf69716d98c6e961
Aaron Gibralter <aaron.gibralter@gmail.com>
2017-05-05 17:14:12 -0400

    wip

--- test.sh
+++ test.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
+npm rm -g yarn 2>/dev/null
+npm install -g yarn@0.23.4 2>/dev/null
+
 rm -rf node_modules
 rm -rf yarn.lock
 rm -rf vendor/npm_cache
 
-npm rm -g yarn
-npm install -g yarn@0.23.4
+function test() {
+    cp .yarnrc-$1 .yarnrc
+    cp package-$2.json package.json
 
-echo "initial install"
-yarn cache clean
-yarn
+    # echo ""
+    # echo "initial install"
+    # yarn cache clean
+    # yarn
 
-rm -rf node_modules
+    # rm -rf node_modules
 
-echo "offline install"
-yarn cache clean
-yarn --offline
+    # echo ""
+    # echo "offline install"
+    # yarn cache clean
+    # yarn --offline
 
+    rm -rf package.json
+    rm -rf .yarnrc
+}
+
+test "with-pruning" "simple"
+test "without-pruning" "simple"
+
+test "with-pruning" "git"
+test "without-pruning" "git"
+
+test "with-pruning" "with-npm-scope"
+test "without-pruning" "with-npm-scope"
