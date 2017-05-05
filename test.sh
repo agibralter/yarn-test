4395c25fffb592082a40a818e5001455fcb3b50c
Aaron Gibralter <aaron.gibralter@gmail.com>
2017-05-05 17:20:55 -0400

    Wip

--- test.sh
+++ test.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-npm rm -g yarn 2>/dev/null
-npm install -g yarn@0.23.4 2>/dev/null
-
-rm -rf node_modules
-rm -rf yarn.lock
-rm -rf vendor/npm_cache
+npm rm -g yarn >/dev/null 2>&1
+npm install -g yarn@0.23.4 >/dev/null 2>&1
 
 function test() {
+    echo "Running: $1 $2"
+
+    rm -rf node_modules
+    rm -rf yarn.lock
+    rm -rf vendor/npm_cache
+
     cp .yarnrc-$1 .yarnrc
     cp package-$2.json package.json
 
-    # echo ""
-    # echo "initial install"
-    # yarn cache clean
-    # yarn
+    echo ""
+    echo "initial install"
+    yarn cache clean
+    yarn
 
-    # rm -rf node_modules
+    rm -rf node_modules
 
-    # echo ""
-    # echo "offline install"
-    # yarn cache clean
-    # yarn --offline
+    echo ""
+    echo "offline install"
+    yarn cache clean
+    yarn --offline
 
     rm -rf package.json
     rm -rf .yarnrc
@@ -33,5 +35,5 @@ test "without-pruning" "simple"
 test "with-pruning" "git"
 test "without-pruning" "git"
 
-test "with-pruning" "with-npm-scope"
-test "without-pruning" "with-npm-scope"
+test "with-pruning" "npm-scope"
+test "without-pruning" "npm-scope"
